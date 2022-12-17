6bcddf496575e54568a2562c33c5958aab00902d
Leshan <leshan0627@gmail.com>
2022-12-16 21:04:12 -0500

    docs: Implement Dashboard.js and styles

--- app.vediai/ClientApp/src/components/Dashboard/Dashboard.js
+++ app.vediai/ClientApp/src/components/Dashboard/Dashboard.js
@@ -8,16 +8,58 @@ export function Dashboard() {
         <div className='dashboard-main--nav'></div>
         <div className='dashboard-main--body'>
           <div className='dashboard-main--body-profile'></div>
-          <div className='dashboard-main--body-profile-second'>
+          <div className='dashboard-main--body-profile-second grid grid-one'>
             <div className='dashboard-main--body-profile-second-banner'>
               <div className='dashboard-main--body-profile-second-banner-text'>
                 <h2>Your teammates are online</h2>
-                <p>
+                <label>
                   Schedule your call, conference meeting or start a new chats
                   with a welcome message
-                </p>
+                </label>
               </div>
               <div className='dashboard-main--body-profile-second-banner-image'></div>
+            </div>
+            <div className='dashboard-main--body-profile-second-body grid grid-two grid-m'>
+              <div className='dashboard-main--body-profile-second-body-events'>
+                <div className='events--text'>
+                  <strong>
+                    <p>Upcoming events</p>
+                  </strong>
+                  <label>Scheduled meetings and calls</label>
+                </div>
+              </div>
+              <div className='dashboard-main--body-profile-second-body-actions'>
+                <div className='dashboard-main--body-profile-second-body-actions-quick-actions'>
+                  <div className='quick-actions--text'>
+                    <strong>
+                      <p>Quick actions</p>
+                    </strong>
+                    <label>Plan a meeting or start a call</label>
+                  </div>
+                  <div className='quick-actions--sections grid grid-two grid-s'>
+                    <div className='quick-actions--sections-action'>
+                      <a href='#'> Start a conference call</a>
+                    </div>
+                    <div className='quick-actions--sections-action'>
+                      <a href='#'> Add new contact</a>
+                    </div>
+                    <div className='quick-actions--sections-action'>
+                      <a href='#'> Send private message</a>
+                    </div>
+                    <div className='quick-actions--sections-action'>
+                      <a href='#'> Launch screenshare</a>
+                    </div>
+                  </div>
+                </div>
+                <div className='dashboard-main--body-profile-second-body-actions-top-users'>
+                  <div className='top-users--text'>
+                    <strong>
+                      <p>Quick actions</p>
+                    </strong>
+                    <label>Plan a meeting or start a call</label>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
